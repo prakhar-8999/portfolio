94f8139681dd45d0b576d560d212f6e7d35c3b0d
prakhar porwal <prakharporwal12a@gmail.com>
2023-10-24 08:47:45 +0000

    feat: dark mode setup

---
+++ components/ToggleTheme.tsx
@@ -0,0 +1,27 @@
+"use client";
+import {useState} from "react";
+import {DarkModeSwitch} from "react-toggle-dark-mode";
+
+const ToogleTheme = () => {
+  const [isDarkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = (checked: boolean) => {
+    if (checked) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    setDarkMode(checked);
+  };
+  return (
+    <div className="absolute bottom-0 right-12">
+      <DarkModeSwitch
+        style={{marginBottom: "2rem"}}
+        checked={isDarkMode}
+        onChange={toggleDarkMode}
+        size={30}
+      />
+    </div>
+  );
+};
+
+export default ToogleTheme;
