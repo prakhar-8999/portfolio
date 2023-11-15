204e20831546c70f0e703ca83dfdefdd72fbcf04
prakhar porwal <prakharporwal12a@gmail.com>
2023-11-15 07:21:29 +0000

    feat: updating color pannel

--- libs/colors.ts
+++ libs/colors.ts
@@ -1,6 +1,6 @@
 "use client";
 
-const webColors = ["#e33e3e", "#220af7", "#22d3ee", "#818cf8", "#8b5cf6"];
+const webColors = ["#e33e3e", "#22d3ee", "#818cf8", "#8b5cf6"];
 
 const colors = {
   primary: webColors[Math.floor(Math.random() * webColors.length)],
