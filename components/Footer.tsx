ad4f6ceeb5162e18e162bd22d241ed05721a2dea
prakhar porwal <prakharporwal12a@gmail.com>
2023-11-21 21:34:26 +0530

    fix: removed twitter

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           {footer.tagLine}
           <a
-            href="https://twitter.com/knyttneve"
+            href={footer.twitter}
             className="text-gray-600 ml-1"
             rel="noopener noreferrer"
             target="_blank"
