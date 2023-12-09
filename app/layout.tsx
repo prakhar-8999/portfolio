dc08581f2b8a41c51d3c219947060962cab27fa3
prakhar porwal <prakharporwal12a@gmail.com>
2023-12-09 19:22:55 +0000

    feat: added vercel speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import {profile} from "@/utils/profile";
+import {SpeedInsights} from "@vercel/speed-insights/next";
 import type {Metadata} from "next";
 import "./globals.css";
 
@@ -13,7 +14,10 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
       <head>
         <link rel="manifest" href="/manifest.json" />
       </head>
-      <body className="bg-white dark:bg-black">{children}</body>
+      <body className="bg-white dark:bg-black">
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
